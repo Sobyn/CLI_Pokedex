84b4ce13b660b23e1a09e8ca28208d9392082d9e
Even Fosser <evenfosser95@gmail.com>
2019-02-11 23:45:39 +0100

    Intro message, starting off with argument parsing

--- pokedex.py
+++ pokedex.py
@@ -7,22 +7,31 @@
 
 import requests
 import json
+import argparse as ap
+
+url = requests.get('https://pokeapi.co/api/v2/')
+jData = url.json()
+
+parser = ap.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-n')
+group.add_argument()
+result = parser.parse_args()
 
 
-def main():
-    url = requests.get('https://pokeapi.co/api/v2/pokemon/charizard')
-    jData = url.json()
-    print("Welcome to the Pokedex!")
-    print("With this program, you can search for"
-          "a great amount of Pokemon-related information!")
-    print("As of right now, you can search within the following topics:")
-    print(jData)
-#    inp = input("What would you like to search for?\n$> ")
-#
-#    print("The response contains {0} properties".format(len(jData)))
-#    print("\n")
-#    print(jData['ability'])
+def intro():
+    print("\nWelcome to the Pokedex!\n"
+          
+          "\nWith the Pokedex, you can search through \n"
+          "a vast collection of Pokemon-related information!\n"
+          
+          "\nYou're seeing this message because you started the \n"
+          "program without any flags specified.\n"
+          
+          "\nFeel free to restart the program, \n"
+          "but this time include the '-h' flag.\n"
+          "\nExample:\t'python pokedex.py -h'")
 
 
 if __name__ == '__main__':
-    main()
+    intro()
